f2bc0626996d6c56a09859c1404d1920750c0df6
Nmazil-Dev <nmazil68@gmail.com>
2021-08-23 14:47:33 -0500

    Added part3 folder and third element exercise

---
+++ part3/third_element.java
@@ -0,0 +1,12 @@
+package part3;
+import java.util.ArrayList;
+public class third_element{
+    public static void main(String[] args){
+        ArrayList <String> list = new ArrayList<>();
+        list.add("Tom");
+        list.add("Emma");
+        list.add("Alex");
+        list.add("Mary");
+
+    }
+}
