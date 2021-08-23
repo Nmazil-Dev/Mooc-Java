5dc86927b993fe38da4b7a1e33c80bb923a0a7b7
Lets-Learn-Coding <nmazil68@gmail.com>
2021-08-22 19:39:44 -0500

    Updated progress.py link

--- progress.py
+++ progress.py
@@ -1,4 +1,4 @@
 #Python script to open mooc @ current progress
 import webbrowser
-mooc = "https://java-programming.mooc.fi/part-3"
+mooc = "https://java-programming.mooc.fi/part-3/2-lists"
 webbrowser.open(mooc)
